7c2ba8acd333d8991baa69604d6d38da17185e69
Melvin Fallas <melfallas@yahoo.com>
2021-05-08 09:45:54 -0600

    E. Adding base project files.

---
+++ src/test/java/com/mb/base/atp/AtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.base.atp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
