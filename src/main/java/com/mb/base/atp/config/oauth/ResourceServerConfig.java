7ffcfcb09b7efa5c961c69328003ec5c61b3e1f3
Melvin Fallas <melfallas@yahoo.com>
2021-05-09 22:17:49 -0600

    E. Resource Server and Security Config changes.

--- src/main/java/com/mb/base/atp/config/oauth/ResourceServerConfig.java
+++ src/main/java/com/mb/base/atp/config/oauth/ResourceServerConfig.java
@@ -19,37 +19,45 @@ import org.springframework.web.filter.CorsFilter;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+//	@Override
+//	public void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//		//.antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**", "/images/**").permitAll()
+//		.antMatchers(HttpMethod.GET, "/api/test1", "/api/test3", "/actuator/health/**").permitAll()
+//		/*.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
+//		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
+//		.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
+//		.antMatchers("/api/clientes/**").hasRole("ADMIN")*/
+//		.anyRequest().authenticated()
+//		.and().cors().configurationSource(corsConfigurationSource());
+//	}
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		//.antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**", "/images/**").permitAll()
 		.antMatchers(HttpMethod.GET, "/api/test1", "/api/test3", "/actuator/health/**").permitAll()
-		/*.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
-		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
-		.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
-		.antMatchers("/api/clientes/**").hasRole("ADMIN")*/
 		.anyRequest().authenticated()
-		.and().cors().configurationSource(corsConfigurationSource());
+		;
 	}
 	
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		config.setAllowCredentials(true);
-		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-		
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		return source;
-	}
-	
-	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter(){
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//		config.setAllowCredentials(true);
+//		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+//		
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", config);
+//		return source;
+//	}
+//	
+//	@Bean
+//	public FilterRegistrationBean<CorsFilter> corsFilter(){
+//		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
 	
 }
