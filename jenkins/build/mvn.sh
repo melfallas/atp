50870e9913d9d6e9471e09ad3a22849c4f0e5d8b
Melvin Fallas <melfallas@yahoo.com>
2021-10-21 20:09:39 -0600

    E. Adding gradle wrapper container for build execution.

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -17,11 +17,12 @@ else
 	echo "****************"
 	echo "* Building jar!*"
 	echo "****************"
-	echo ""
+	echo "$PWD"
 	#docker run --rm -v /root/.m2:/root/.m2 -v $appDir:/app -w /app maven:3-alpine "$mvnComand1" "$mvnComand2"
 	#docker run --rm -v /root/.m2:/root/.m2 -v $appDir:/app -w /app maven:3-alpine "$mvnComand1" "$mvnComand2" "$mvnComand3" "$mvnComand4" "$mvnComand5"
-	cd ${appDir}
-	gradlew build -x test
+	cd /app
+	docker run --rm -u gradle -v /home/admin1/jenkins/jenkins_home/workspace/pipeline-maven-test:/app -w /app gradle:6.9-jdk8 /usr/bin/gradle wrapper build -x test
+	#docker run --rm -u gradle -v $appDir:/app -w /app gradle:jdk8-alpine gradlew build -x test
 fi
 
 
