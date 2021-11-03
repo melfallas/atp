a66b0f3692b653b9c1df500c671f1d1e0adabfc1
Melvin Fallas <melfallas@yahoo.com>
2021-11-02 21:07:14 -0600

    E. Fixing tty error.

--- jenkins/routing/gendomain.sh
+++ jenkins/routing/gendomain.sh
@@ -69,7 +69,7 @@ echo "|"
 echo "Reloading proxy server"
 echo "|"
 #cd $ROUTING_DIR && docker-compose -f docker-compose-nginx-ssl-proxy.yml exec nginx-ssl-proxy-server nginx -s reload
-docker-compose -f docker-compose-nginx-ssl-proxy.yml -p WebProxyServer exec --tty nginx-ssl-proxy-server nginx -s reload
+docker-compose -f docker-compose-nginx-ssl-proxy.yml -p WebProxyServer exec -T nginx-ssl-proxy-server nginx -s reload
 echo "|"
 echo "Finish ..."
 echo "|"
