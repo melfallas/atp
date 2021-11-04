f722a46b276e4ab47525a083523dcb55624cf326
Melvin Fallas <melfallas@yahoo.com>
2021-11-03 19:22:17 -0600

    E. Adding domain creation by API invocation.

--- jenkins/routing/gendomain.sh
+++ jenkins/routing/gendomain.sh
@@ -2,7 +2,7 @@
 
 export ROUTING_DIR="/var/jenkins_home/workspace/pipeline-maven-test/jenkins/routing"
 
-DGTOKEN=$1
+DNS_API_TOKEN=$1
 
 export IMAGE=$(sed -n '1p' /tmp/.auth)
 export TAG=$(sed -n '2p' /tmp/.auth)
@@ -10,6 +10,8 @@ export CONTAINER_NAME=$(sed -n '3p' /tmp/.auth)
 export ENV=$(sed -n '4p' /tmp/.auth)
 export HOST_PORT=$(sed -n '5p' /tmp/.auth)
 export DOCKER_PORT=$(sed -n '6p' /tmp/.auth)
+
+export SERVER_IP="157.230.153.157"
 
 # Default constanst
 SUBDOMAIN=$CONTAINER_NAME-$ENV
@@ -21,39 +23,67 @@ CONFIG_PATH="/var/nginx/conf"
 
 #SUBDOMAIN_PATTERM="app1-ist"
 #SUBDOMAIN_PATTERM="name:app1-ist,"
-SUBDOMAIN_PATTERM="\"name\":\"app1-ist\","
 
-# Create server name string
+# Create needed string variables
 SERVER_NAME=$SUBDOMAIN.$DOMAIN
 PROXY_LOCATION=http://$DOMAIN:$PROXY_PORT
-
+SUBDOMAIN_PATTERM="\"name\":\"app1-ist\","
 CONF_FILE_NAME=$SERVER_NAME.conf
+
+echo ""
+echo "############################"
+echo "*** Procesing Web Domain ***"
+echo "############################"
+echo ""
 echo "|"
-echo "Token used: $DGTOKEN"
+echo "Registered API Token: $DNS_API_TOKEN"
 echo "|"
 echo "Consulting existing domains..."
 echo "|"
-existing_domains=$(curl -X GET \
+DOMAIN_MATCH_LIST=$(curl -X GET \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $DGTOKEN" \
+  -H "Authorization: Bearer $DNS_API_TOKEN" \
   "https://api.digitalocean.com/v2/domains/$DOMAIN/records?type=A&name=$SERVER_NAME"
   )
 #  "https://api.digitalocean.com/v2/domains/mbsoftware.ml/records?type=A&name=app1-ist.mbsoftware.ml"
-  
-echo "$existing_domains"
+
 echo "|"
-#echo "$existing_domains" | grep $SUBDOMAIN_PATTERM
+echo "Domain match list:"
+echo "---"
+echo "$DOMAIN_MATCH_LIST"
+echo "---"
+echo "|"
+#echo "$DOMAIN_MATCH_LIST" | grep $SUBDOMAIN_PATTERM
 #echo "|"
-DOMAIN_MATCH=$(echo "$existing_domains" | grep $SUBDOMAIN_PATTERM)
-#echo "Match: $DOMAIN_MATCH"
-if [ -z "$DOMAIN_MATCH" ]
+DOMAIN_RECORD=$(echo "$DOMAIN_MATCH_LIST" | grep $SUBDOMAIN_PATTERM)
+#echo "Match: $DOMAIN_RECORD"
+if [ -z "$DOMAIN_RECORD" ]
 then
-      echo "Not existing domain: $SERVER_NAME"
-      echo "Creating domain by API endpoint..."
+    echo "Not existing domain: $SERVER_NAME"
+    echo "Creating domain by API endpoint..."
+	CREATE_DOMAIN_RESULT=$(curl -X POST \
+	  -H "Content-Type: application/json" \
+	  -H "Authorization: Bearer $DNS_API_TOKEN" \
+	  -d '{"type":"A","name":"$SUBDOMAIN","data":"$SERVER_IP","priority":null,"port":null,"ttl":1800,"weight":null,"flags":null,"tag":null}' \
+	  "https://api.digitalocean.com/v2/domains/$DOMAIN/records"
+	  )
+	echo "|"
+	echo "Domain creation result:"
+	echo "---"
+	echo "$CREATE_DOMAIN_RESULT"
+	echo "---"
+	echo "|"
 else
-      echo "Establish existing domain: $SERVER_NAME"
+      echo "Domain alredy existing: $SERVER_NAME"
+	  echo "No additional operations needed..."
 fi
 #exit 1
+echo "|"
+echo ""
+echo "#############################"
+echo "*** Configuring Web Proxy ***"
+echo "#############################"
+echo ""
 echo "|"
 echo "Executing by user: "
 whoami
@@ -99,7 +129,7 @@ echo "|"
 cd $ROUTING_DIR
 echo "Changing path to routing directory: $PWD"
 echo "|"
-echo "Reloading proxy server"
+echo "***** Reloading proxy server *****"
 echo "|"
 #cd $ROUTING_DIR && docker-compose -f docker-compose-nginx-ssl-proxy.yml exec nginx-ssl-proxy-server nginx -s reload
 docker-compose -f docker-compose-nginx-ssl-proxy.yml -p WebProxyServer exec -T nginx-ssl-proxy-server nginx -s reload
